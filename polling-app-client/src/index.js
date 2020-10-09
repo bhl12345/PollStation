90f717b2522bd1c6bbc91882f25445dab4dbee3b
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-10-09 10:00:25 -0400

    changed up color scheme

--- polling-app-client/src/index.js
+++ polling-app-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import App from './app/App';
 ReactDOM.render(
     <Router>
         <App />
